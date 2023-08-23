e4c772cf64d22e0ae336e98b2e7510211b31b048
Jaewoong <dixk3458@naver.com>
2023-08-23 15:08:51 +0900

    3.loading(여러가지 유니온 타입의 접근)

---
+++ 2.Type-Projects/3.loading.ts
@@ -0,0 +1,37 @@
+type LoadingState = {
+  state: 'loading';
+};
+
+type SuccessState = {
+  state: 'success';
+  response: {
+    body: string;
+  };
+};
+
+type FailState = {
+  state: 'fail';
+  reason: string;
+};
+
+type ResourceLoadState = LoadingState | SuccessState | FailState;
+
+function printLoginState(state: ResourceLoadState): void {
+  switch (state.state) {
+    case 'loading':
+      console.log(`👀 ${state.state}...`);
+      break;
+    case 'success':
+      console.log(`😊 ${state.response.body}`);
+      break;
+    case 'fail':
+      console.log(`😱 ${state.reason}`);
+      break;
+    default:
+      throw new Error(`unknown state: ${state}`);
+  }
+}
+
+printLoginState({ state: 'loading' });
+printLoginState({ state: 'success', response: { body: 'loaded' } });
+printLoginState({ state: 'fail', reason: 'no network' });
