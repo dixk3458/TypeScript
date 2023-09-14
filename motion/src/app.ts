3aaa6e4d3e2e40a9fe16574d35f6df7af956a671
Jaewoong <dixk3458@naver.com>
2023-09-14 21:57:42 +0900

    4.ItemComponent

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,4 +1,7 @@
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -11,6 +14,16 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+    const todo = new TodoComponent('Todo Title', 'Todo');
+    todo.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://youtu.be/FQZhehVRSXQ'
+    );
+    video.attachTo(appRoot, 'beforeend');
   }
 }
 
