c74168c8e233a93012cb8e824bf443e89bf237af
Jaewoong <dixk3458@naver.com>
2023-09-14 23:32:13 +0900

    5.PageItemComponent

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,29 +1,33 @@
+import { Component } from './components/component.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
     const image = new ImageComponent(
       'Image Title',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
     const note = new NoteComponent('Note Title', 'Note Body');
-    note.attachTo(appRoot, 'beforeend');
+
     const todo = new TodoComponent('Todo Title', 'Todo');
-    todo.attachTo(appRoot, 'beforeend');
 
     const video = new VideoComponent(
       'Video Title',
       'https://youtu.be/FQZhehVRSXQ'
     );
-    video.attachTo(appRoot, 'beforeend');
+
+    this.page.addChild(image);
+    this.page.addChild(note);
+    this.page.addChild(todo);
+    this.page.addChild(video);
   }
 }
 
