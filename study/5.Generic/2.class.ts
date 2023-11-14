16c3ce05aff10924bd257f1e35d5d736540361d6
Jaewoong <dixk3458@naver.com>
2023-11-14 15:38:29 +0900

    Edit Generic Class

--- study/5.Generic/2.class.ts
+++ study/5.Generic/2.class.ts
@@ -1,37 +1,22 @@
-interface Either {
-  left: () => number;
-  right: () => number;
-}
+// 클래스에서의 제네릭
 
-// SimpleEither로 만들어진 인스턴스는 숫자를 받아서 숫자만 리턴을 해준다.
-// left,right가 숫자만 받아서 리턴을 하는데, 숫자가 아닌 다른 타입을 받고싶을때는 어떻게해야할까?
-// 또는 서로 다른 타입을 리턴 하고싶을때는 어떻게해야할까?
-
-class SimpleEither implements Either {
-  constructor(private leftValue: number, private rightValue: number) {}
-  left(): number {
-    return this.leftValue;
-  }
-
-  right(): number {
-    return this.rightValue;
-  }
-}
-
-interface EitherGeneric<L, R> {
+interface Either<L, R> {
   left: () => L;
   right: () => R;
 }
 
-class GenericEither<L, R> implements EitherGeneric<L, R> {
+class SimpleEither<L, R> implements Either<L, R> {
   constructor(private leftValue: L, private rightValue: R) {}
-  left() {
+
+  left(): L {
     return this.leftValue;
   }
-  right() {
+
+  right(): R {
     return this.rightValue;
   }
 }
 
-const generic = new GenericEither(123, 'String');
-console.log(generic);
+const either = new SimpleEither('Left', 123);
+console.log(either.left());
+console.log(either.right());
